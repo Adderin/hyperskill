284f01b911782abae43e1ead708d0caa53070176
Adderin <adderin@gmail.com>
2020-01-04 11:09:25 +0100

    hyperskill.org - Relational operators

--- src/firstwave/CheckTheValue.java
+++ src/firstwave/CheckTheValue.java
@@ -1,4 +1,11 @@
 package firstwave;
 
-public class CheckTheValue {
+import java.util.Scanner;
+
+public class CheckTheValue  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println(num < 10 && num > 0);
+    }
 }
