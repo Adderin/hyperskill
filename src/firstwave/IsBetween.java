577884c19b9a11fb06638283728cac230c011691
Adderin <adderin@gmail.com>
2020-01-04 21:30:29 +0100

    hyperskill.org - several pushes

---
+++ src/firstwave/IsBetween.java
@@ -0,0 +1,13 @@
+package firstwave;
+
+import java.util.Scanner;
+
+public class IsBetween {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println((a <= c && a >= b) || (a >= c && a <= b));
+    }
+}
