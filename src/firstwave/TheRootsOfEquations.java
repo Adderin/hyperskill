45a90eeccef0b46b212088877a3f46efb2ffaf90
Adderin <adderin@gmail.com>
2020-01-06 20:34:44 +0100

    hyperskill.org - The roots of an equation

---
+++ src/firstwave/TheRootsOfEquations.java
@@ -0,0 +1,20 @@
+package firstwave;
+
+import java.util.Scanner;
+
+public class TheRootsOfEquations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        for(int x = 1; x <= 1000; x++){
+            if(a * x * x * x + b * x * x + c * x + d == 0){
+                System.out.println(x);
+            }
+        }
+    }
+}
