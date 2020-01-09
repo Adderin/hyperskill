6ec9030d097707a78875adc96af02c53ad56f462
Adderin <adderin@gmail.com>
2020-01-09 18:16:08 +0100

    hyperskill.org - Calculate Profit

--- src/firstwave/CalculateProfit.java
+++ src/firstwave/CalculateProfit.java
@@ -1,4 +1,18 @@
 package firstwave;
 
+import java.util.Scanner;
+
 public class CalculateProfit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double m = scanner.nextDouble();  // money at current time
+        double p = scanner.nextDouble();  // percentage given
+        double k = scanner.nextDouble();  // money after percentage given
+        int years = 0;
+        while(m < k){
+            m = m * (1 + 0.01*p);
+        years++;
+        }
+        System.out.println(years);
+    }
 }
