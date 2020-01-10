49ba7877a06ad894d21c3c4f8c04d9535e4e88ad
Adderin <adderin@gmail.com>
2020-01-10 16:29:22 +0100

    hyperskill.org - Even or odd, ver.2

--- src/firstwave/EvenOrOdd.java
+++ src/firstwave/EvenOrOdd.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class EvenOrOdd {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int num = 1;
+        int num;
         String str = "";
-        while (num > 0) {
+        while (scanner.hasNext()) {
             num = scanner.nextInt();
             if (num == 0) {
                 break;
-            } else if (num % 2 == 1) {
+            }
+            if (num % 2 == 1) {
                 str += "odd\n";
-            } else if(num % 2 == 0) {
+            } else {
                 str += "even\n";
             }
         }
