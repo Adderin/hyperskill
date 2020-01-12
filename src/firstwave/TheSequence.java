90a8656c683592c27b0833d3ab438759e9e48c64
Adderin <adderin@gmail.com>
2020-01-12 19:48:32 +0100

    hyperskill.org - The sequence

--- src/firstwave/TheSequence.java
+++ src/firstwave/TheSequence.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class TheSequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int num = scanner.nextInt();
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+                count++;
+                if(count == num) {
+                    return;
+                }
+            }
+        }
     }
 }
