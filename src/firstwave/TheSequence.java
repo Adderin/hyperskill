1c7309b9124510bf5ec932ddfae852dfa24ee12a
Adderin <adderin@gmail.com>
2020-01-12 16:13:27 +0100

    hyperskill.org - The sequence

---
+++ src/firstwave/TheSequence.java
@@ -0,0 +1,10 @@
+package firstwave;
+
+import java.util.Scanner;
+
+public class TheSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
