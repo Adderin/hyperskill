9f5f5d5751cb184985a65d4bd29b0c87beb32798
Adderin <adderin@gmail.com>
2020-01-14 21:21:35 +0100

    hyperskill.org - Raise to the power

---
+++ src/firstwave/RaiseToPower.java
@@ -0,0 +1,18 @@
+package firstwave;
+
+import java.util.Scanner;
+
+public class RaiseToPower {
+
+    public static long power(int n, int m) {
+        return (long) Math.pow(n, m);
+    }
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        final int m = scanner.nextInt();
+        System.out.println(power(n, m));
+    }
+}
+
